00b5a4bedc19fc6a869341b364ebdb89ce8d8a44
Bharat Paliwal <18uec169@lnmiit.ac.in>
2022-03-26 01:13:57 +0530

    mvc work

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,33 @@
+import PostMessage from '../models/postMessage.js'
+
 // application logic is written here
-export const getPosts = (req,res) => { 
-  res.send("This is working");
+
+// export const getPosts = (req,res) => { 
+//   res.send("This is working");
+// };
+
+export const getPosts = async (req,res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    console.log(postMessages);
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({message: error.message});
+  }
+  
 };
+
+export const createPost = async(req, res) => {
+  
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+
+    res.status(200).json(newPost);
+  } catch (error) {
+    res.status(404).json({message: error.message});
+  }
+}
